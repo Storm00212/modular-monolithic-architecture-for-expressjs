9c864d8480ce334b4bc1fe5aa71b90d33698042c
Storm00212 <paulmuyalikhams@gmail.com>
2026-05-17 17:58:37 +0300

    route

---
+++ src/modules/auth/auth.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import {
+  registerController
+} from "./auth.controller";
+
+const router = Router();
+
+router.post(
+  "/register",
+  registerController
+);
+
+export default router;
