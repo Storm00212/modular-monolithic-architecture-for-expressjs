9024836f9d6116f7978088fdc5f0f92b554c748d
Storm00212 <paulmuyalikhams@gmail.com>
2026-05-27 16:45:44 +0300

    changes

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,5 +1,22 @@
-import { getUsers } from "./user.repository";
+import {
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+} from "./user.repository";
 
 export const fetchUsers = async () => {
   return getUsers();
+};
+
+export const findUserById = async (id: string) => {
+  return getUserById(id);
+};
+
+export const updateUserService = async (id: string, data: { name?: string; email?: string }) => {
+  return updateUser(id, data);
+};
+
+export const deleteUserService = async (id: string) => {
+  return deleteUser(id);
 };
