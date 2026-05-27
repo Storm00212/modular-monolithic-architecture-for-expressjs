dc09acb6883733366cc32e0d60bd3fcea466dfc4
Storm00212 <paulmuyalikhams@gmail.com>
2026-05-27 16:45:21 +0300

    changes

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
+import { authenticate } from "../../core/middleware/auth.middleware";
+import { authorize } from "../../core/middleware/authorize.middleware";
 
 import {
-  getUsersController
+  getUsersController,
+  getUserByIdController,
+  updateUserController,
+  deleteUserController
 } from "./user.controller";
 
 const router = Router();
 
-router.get("/", getUsersController);
+router.get("/", authenticate, getUsersController);
+router.get("/:id", authenticate, getUserByIdController);
+router.patch("/:id", authenticate, authorize(["ADMIN"]), updateUserController);
+router.delete("/:id", authenticate, authorize(["ADMIN"]), deleteUserController);
 
 export default router;
