6a93bb24e4d52592227458b2f784e5224df92a15
Storm00212 <paulmuyalikhams@gmail.com>
2026-05-27 16:43:17 +0300

    changes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
+import { authenticate } from "../../core/middleware/auth.middleware";
 
 import {
-  registerController
+  registerController,
+  loginController,
+  getProfileController
 } from "./auth.controller";
 
 const router = Router();
 
-router.post(
-  "/register",
-  registerController
-);
+router.post("/register", registerController);
+router.post("/login", loginController);
+router.get("/profile", authenticate, getProfileController);
 
 export default router;
